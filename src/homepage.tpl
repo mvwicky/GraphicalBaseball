ebd06f783ffe2fadfcb2999084ae796162729f39
Michael Van Wickle <mvanwickle@gmail.com>
2016-08-03 20:57:58 -0400

    initial server test

---
+++ src/homepage.tpl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Graphical Baseball</title>
+</head>
+<body>
+	Hello {{ name }}
+	<br>
+	{{ date }}
+</body>
+</html>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use] extern crate nickel;
+
+use std::collections::HashMap;
+use nickel::{Nickel, HttpRouter};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.get("/", middleware! { |_, response|
+    	let mut data = HashMap::new();
+    	data.insert("name", "Michael");
+    	data.insert("date", "8-3-2016");
+    	return response.render("src/homepage.tpl", &data);
+    });
+
+    server.listen("127.0.0.1:6767");
 }
